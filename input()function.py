d14d0f6427c33dce53c7390785b0a87aec31990c
Tejasri.Noone <132540661+TejasriNoone@users.noreply.github.com>
2023-05-05 12:10:51 +0530

    Add files via upload

---
+++ input()function.py
@@ -0,0 +1,34 @@
+##input("what's your name")
+
+##input("what's your name")
+##print("Hi Teju")
+
+##input("what's your name")
+##print('Hi Teju')
+
+##input("what's your name")
+##print('Hi (input)')
+
+##a=input("what's your name?")
+##print("hi",a)
+
+##a=input("what's your name?")
+##print(a,"hi")
+
+##print("hello"+" "+input("what's your name"))
+
+##print('hello'+input("what's your name"))
+
+print("hey "+input("what's your name? ")+" ," +",how are you?")
+
+
+
+
+
+
+
+
+
+
+
+
